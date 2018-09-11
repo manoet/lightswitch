25b7c9f2a4a131fd916fa2961061005be4f7af3d
manoet <manoet.ita@gmail.com>
2018-09-11 22:14:00 +0200

    Latch tests

--- test/ts_latch.cpp
+++ test/ts_latch.cpp
@@ -19,3 +19,93 @@
  * SOFTWARE.
  */
 
+#include "lightswitch/latch.hpp"
+#include "catch2/catch.hpp"
+#include <thread>
+
+constexpr std::size_t latch_size = 3;
+
+SCENARIO("Initialization", "[latch]") {
+    lightswitch::latch latch(latch_size);
+    GIVEN("a latch") {
+        WHEN("latch is built") {
+            THEN("count() is equal to the given count") {
+                REQUIRE(latch.count() == latch_size);
+            }
+        }
+    }
+}
+
+SCENARIO("Counting down", "[latch]") {
+    lightswitch::latch latch(latch_size);
+    REQUIRE(latch.count() == latch_size);
+    GIVEN("a latch") {
+        WHEN("would_block() is invoked") {
+            THEN("it returns true") {
+                REQUIRE(latch.would_block());
+            }
+        }
+        WHEN("count_down is invoked") {
+            latch.count_down();
+            THEN("count() is decreased") {
+                REQUIRE(latch.count() == latch_size - 1);
+            }
+        }
+        WHEN("count reaches zero") {
+            std::thread t([&latch](){latch.wait();});
+            for (auto i=0; i<latch_size; ++i) {
+                latch.count_down();
+            }
+            THEN("threads are unlocked") {
+                t.join();
+                REQUIRE(true);
+            }
+            THEN("would_block() returns false") {
+                REQUIRE_FALSE(latch.would_block());
+            }
+            WHEN("count_down invoked again") {
+                latch.count_down();
+                THEN("count_down has no effect") {
+                    REQUIRE(latch.count() == 0);
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("Reset mechanism", "[latch]") {
+    GIVEN("a latch with count to 0") {
+        lightswitch::latch latch(latch_size);
+        for (auto i=0; i<latch_size; ++i) {
+            latch.count_down();
+        }
+        WHEN("reset() is invoked") {
+            latch.reset();
+            THEN("count is restored") {
+                REQUIRE(latch.count() == latch_size);
+            }
+        }
+        WHEN("reset(N) is invoked") {
+            auto new_count = latch_size + 3;
+            latch.reset(new_count);
+            THEN("count is N") {
+                REQUIRE(latch.count() == new_count);
+            }
+        }
+    }
+
+    GIVEN("a thread waiting on the latch") {
+        lightswitch::latch latch(latch_size);
+        std::thread t([&latch](){latch.wait();});
+        WHEN("reset() is invoked") {
+            THEN("an error is thrown") {
+                REQUIRE_THROWS_AS(latch.reset(), std::runtime_error);
+            }
+        }
+        WHEN("reset(N) is invoked") {
+            THEN("an error is thrown") {
+                REQUIRE_THROWS_AS(latch.reset(42), std::runtime_error);
+            }
+        }
+    }
+}
